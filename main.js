2fc874e755b4548c8d65bda47e9b61646778e149
dominictrent <Dominctrent@icloud.com>
2022-10-20 11:23:15 -0600

    java

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 
 
+//input
+ let CSS1 = +prompt("Enter CSS1 Grade");
+ let SP1 = +prompt("Enter SP1 Grade");
+ let SP2= +prompt("Enter SP2 Grade");
+ let CSS2 = +prompt("Enter CSS2 Grade");
+ let PA = +prompt("Enter PA Grade");
+ 
 
+
+
+
+
+//process
+
+let average =(CSS1 + SP1 + SP2 + CSS2 + PA) / 5;
+
+
+
+//output
+alert(`The average grade is: ${average}%.`);
